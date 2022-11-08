5526f81bcbfc972fbf647ebc09d8fe7d0bb74060
XainTheGreat <xainthegreat@yahoo.com>
2022-11-08 14:07:26 -0600

    completed UserRepository tests.

--- src/test/java/com/chat/chat_spring/repository/UserRepositoryTest.java
+++ src/test/java/com/chat/chat_spring/repository/UserRepositoryTest.java
@@ -24,6 +24,7 @@ class UserRepositoryTest {
     @Test
     void shouldFindAll() {
         // arrange
+        userRepoTest.deleteAll();
         List<UserModel> userList = new LinkedList<>();
         userList.add(new UserModel("001", 1, "test", "user", "test_user",
                 "test_password","test_city", "test_state", "test_zipcode", "test_country"));
@@ -42,17 +43,77 @@ class UserRepositoryTest {
 
     @Test
     void shouldFindUserByUserName() {
+        // arrange
+        userRepoTest.deleteAll();
+        UserModel user1 = new UserModel("001", 1, "test", "user", "test_user",
+                "test_password","test_city", "test_state", "test_zipcode", "test_country");
+        UserModel user2 = new UserModel("002", 2, "test2", "user2", "test_user2",
+                "test_password2","test_city2", "test_state2", "test_zipcode2", "test_country2");
+        UserModel user3 = new UserModel("003", 3, "test3", "user3", "test_user3",
+                "test_password3","test_city3", "test_state3", "test_zipcode3", "test_country3");
+        List<UserModel> userList = new LinkedList<>();
+        userList.add(user1);
+        userList.add(user2);
+        userList.add(user3);
+        userRepoTest.saveAll(userList);
+
+        // act
+        List<UserModel> result = userRepoTest.findAll();
+
+        //assert
+        assertEquals(userList, result);
     }
 
     @Test
     void shouldFindUserByUserId() {
+        // arrange
+        userRepoTest.deleteAll();
+        UserModel user1 = new UserModel("001", 1, "test", "user", "test_user",
+                "test_password","test_city", "test_state", "test_zipcode", "test_country");
+        userRepoTest.save(user1);
+
+        // act
+        UserModel result = userRepoTest.findUserByUserId(1);
+
+        // assert
+        assertEquals(user1, result);
     }
 
     @Test
     void shouldFindFirstByUserName() {
+        // arrange
+        userRepoTest.deleteAll();
+        UserModel user1 = new UserModel("001", 1, "test", "user", "test_user",
+                "test_password","test_city", "test_state", "test_zipcode", "test_country");
+        userRepoTest.save(user1);
+
+        // act
+        UserModel result = userRepoTest.findFirstByUserName("test_user");
+
+        // assert
+        assertEquals(user1, result);
     }
 
     @Test
     void shouldFindFirstByOrderByUserIdDesc() {
+        // arrange
+        userRepoTest.deleteAll();
+        UserModel user1 = new UserModel("001", 1, "test", "user", "test_user",
+                "test_password","test_city", "test_state", "test_zipcode", "test_country");
+        UserModel user2 = new UserModel("002", 2, "test2", "user2", "test_user2",
+                "test_password2","test_city2", "test_state2", "test_zipcode2", "test_country2");
+        UserModel user3 = new UserModel("003", 3, "test3", "user3", "test_user3",
+                "test_password3","test_city3", "test_state3", "test_zipcode3", "test_country3");
+        List<UserModel> userList = new LinkedList<>();
+        userList.add(user1);
+        userList.add(user2);
+        userList.add(user3);
+        userRepoTest.saveAll(userList);
+
+        // act
+        UserModel result = userRepoTest.findFirstByOrderByUserIdDesc();
+
+        // assert
+        assertEquals(user3, result);
     }
 }
